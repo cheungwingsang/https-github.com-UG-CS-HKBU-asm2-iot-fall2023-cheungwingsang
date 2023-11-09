4c07b3e610433826a3af29e360bd62eeb6bd8367
Leo Cheung <21217351@life.hkbu.edu.hk>
2023-11-09 23:05:57 +0800

    Part A

---
+++ Assignment2/Preview Content/StaticChart.swift
@@ -0,0 +1,18 @@
+//
+//  StaticChart.swift
+//  Assignment2
+//
+//  Created by Leo Cheung on 3/11/2023.
+//
+
+import SwiftUI
+
+struct StaticChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StaticChart()
+}
